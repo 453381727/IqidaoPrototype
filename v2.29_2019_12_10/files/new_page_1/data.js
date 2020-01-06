1db652aa68786e63421e5214c62f43bedec645c3
453381727 <43769127+453381727@users.noreply.github.com>
2020-01-06 18:02:35 +0800

    版本变更，全删

--- v2.29_2019_12_10/files/new_page_1/data.js
+++
@@ -1,7 +0,0 @@
-﻿$axure.loadCurrentPage(
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j,k,l,m,n,o],p,_(q,r,s,t,u,v,w,_(),x,_(y,z,A,B,C,_(D,E,F,G),H,null,I,B,J,B,K,L,M,null,N,O,P,Q,R,S,T,O),U,_(),V,_(),W,_(X,[])),Y,_(),Z,_());}; 
-var b="url",c="new_page_1.html",d="generationDate",e=new Date(1576483647133.96),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="removeGroup",k="count_num",l="input_count",m="update_count",n="enlarge_pic",o="AxPlusSN",p="page",q="packageId",r="95bcc1b3c7ae4f86a7d1b121236ebae1",s="type",t="Axure:Page",u="name",v="New Page 1",w="notes",x="style",y="baseStyle",z="627587b6038d43cca051c114ac41ad32",A="pageAlignment",B="near",C="fill",D="fillType",E="solid",F="color",G=0xFFFFFFFF,H="image",I="imageHorizontalAlignment",J="imageVerticalAlignment",K="imageRepeat",L="auto",M="favicon",N="sketchFactor",O="0",P="colorStyle",Q="appliedColor",R="fontName",S="Applied Font",T="borderWidth",U="adaptiveStyles",V="interactionMap",W="diagram",X="objects",Y="masters",Z="objectPaths";
-return _creator();
-})());
